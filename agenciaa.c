78959937c5daa19e1c54d2c4ca6664c6fd4fbee4
Henrique Bauer <bauerhenrique12@gmail.com>
2023-06-06 21:16:52 -0300

    Falta a funcao de apagar o contato

--- agenciaa.c
+++ agenciaa.c
@@ -6,13 +6,14 @@ char lista_todos_nomes[LIMIT][LIMIT];
 char lista_todos_sobrenomes[LIMIT][LIMIT];
 char lista_todos_numeros[LIMIT][LIMIT];
 int indice = 0;
-void printaosnomes();
-void printaossobrenomes();
-void printaosnumeros();
+void adicionacontato();
 void alterasnomecontato();
 void alterasobrenomecontato();
 void alteranumerocontato();
-void exibemenucontato();
+void printnatela();
+void exibemenuapagarcontato();
+void buscacontato();
+void exibemenualterarcontato();
 int exibemenu();
 void adicionacontato(){
     char nome[LIMIT];
@@ -32,30 +33,6 @@ void adicionacontato(){
     strcpy(lista_todos_numeros[indice], numerocell);
     indice++;
 }
-void printaosnomes(){
-    int i;
-    for (i = 0; i <= indice; i++)
-    {
-        printf(lista_todos_nomes[i]);
-        printf("\n");
-    }
-}
-void printaossobrenomes(){
-    int i;
-    for (i = 0; i <= indice; i++)
-    {
-        printf(lista_todos_sobrenomes[i]);
-        printf("\n");
-    }
-}
-void printaosnumeros(){
-    int i;
-    for (i = 0; i <= indice; i++)
-    {
-        printf(lista_todos_numeros[i]);
-        printf("\n");
-    }
-}
 void alteranomecontato(int i){
     char novonome[LIMIT];
     printf("Digite o novo nome: ");
@@ -74,7 +51,118 @@ void alteranumerocontato(int i){
     scanf("%s", novonumero);
     strcpy(lista_todos_nomes[i], novonumero);
 }
-void exibemenucontato(){
+void printnatela(){
+    int i;
+    for(i = 0; i < indice; i++)
+    {
+    printf("Nome: %s\n",lista_todos_nomes[i]);
+    printf("Sobrenome: %s\n",lista_todos_sobrenomes[i]);
+    printf("Numero: %s\n",lista_todos_numeros[i]);
+    printf("----------------------------------\n");
+    }
+}
+void exibemenuapagarcontato(){
+    char escolhausuario[LIMIT];
+    int i;
+    int numeroescolhido = 0;
+    char nomeescolhido[LIMIT];
+    char sobrenomeescolhido[LIMIT];
+    char escolhanumerocelular[LIMIT];
+    printf("Voce tem certeza que quer excluir um contato?");
+    printf("S ou N");
+    scanf("%c",escolhausuario);
+    if(escolhausuario == 'S' || escolhausuario == 's'){
+        printf("Vamos excluir o contato!");
+        printf("\n");
+        printf("Digite 1 para procurar o contato pelo nome ou digite 2 se voce quiser procurar pelo nome");
+        scanf("%d",numeroescolhido);
+        if(numeroescolhido == 1){
+            printf("Digite o nome da pessoa:");
+            scanf("%s",nomeescolhido);
+            for(i = 0; i < indice; i++){
+                if(strcmp(nomeescolhido,lista_todos_nomes[i]) == 0){
+                    printf("Nome encotrado!");
+                    printf("\n");
+                    printf("Digite o sobrenome dela: ");
+                    scanf("%s",sobrenomeescolhido);
+                    printf("\n");
+                    if(strcmp(sobrenomeescolhido,lista_todos_sobrenomes[i]) == 0){
+                        //printf("Aqui esta o dados do(a): Nome:%s\nSobrenome:%s\nNumero:%s\n",nomeescolhidobusca,sobrenomeescolhidobusca,lista_todos_numeros[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        else if(numeroescolhido == 2){
+        printf("Digite o numero da pessoa:");
+        scanf("%s",escolhanumerocelular);
+        for(i = 0; i < indice; i++){
+            if(strcmp(escolhanumerocelular,lista_todos_numeros[i]) == 0){
+                printf("Numero encontrado!");
+                printf("\n");
+                //printf("Aqui esta o dados do(a): Nome:%s\nSobrenome:%s\nNumero:%s\n",lista_todos_nomes[i],lista_todos_sobrenomes[i],escolhanumerocelularbusca);
+                break;
+            }
+        }
+    }
+    else{
+        printf("Opcao invalida");
+        printf("\n");
+        printf("Tente novamente");
+        printf("\n");
+    }
+    }
+    else{
+        printf("Tente outra opcao entao!");
+    }
+}
+void buscacontato(){
+    int i;
+    char nomeescolhidobusca[LIMIT];
+    char sobrenomeescolhidobusca[LIMIT];
+    int numerodigitadobusca = 0;
+    char escolhanumerocelularbusca[LIMIT];
+    printf("Vamos buscar o contato desejado!");
+    printf("\n");
+    printf("Digite 1 se voce quiser buscar o contato pelo nome ou digite 2 se quiser procurar pelo numero: ");
+    scanf("%d",&numerodigitadobusca);
+    if(numerodigitadobusca == 1){
+        printf("Digite o nome da pessoa:");
+        scanf("%s",nomeescolhidobusca);
+        for(i = 0; i < indice; i++){
+            if(strcmp(nomeescolhidobusca,lista_todos_nomes[i]) == 0){
+                printf("Nome encotrado!");
+                printf("\n");
+                printf("Digite o sobrenome dela: ");
+                scanf("%s",sobrenomeescolhidobusca);
+                printf("\n");
+                if(strcmp(sobrenomeescolhidobusca,lista_todos_sobrenomes[i]) == 0){
+                    printf("Aqui esta o dados do(a): Nome:%s\nSobrenome:%s\nNumero:%s\n",nomeescolhidobusca,sobrenomeescolhidobusca,lista_todos_numeros[i]);
+                    break;
+                }
+            }
+        }
+    }
+    else if(numerodigitadobusca == 2){
+        printf("Digite o numero da pessoa:");
+        scanf("%s",escolhanumerocelularbusca);
+        for(i = 0; i < indice; i++){
+            if(strcmp(escolhanumerocelularbusca,lista_todos_numeros[i]) == 0){
+                printf("Numero encontrado!");
+                printf("\n");
+                printf("Aqui esta o dados do(a): Nome:%s\nSobrenome:%s\nNumero:%s\n",lista_todos_nomes[i],lista_todos_sobrenomes[i],escolhanumerocelularbusca);
+                break;
+            }
+        }
+    }
+    else{
+        printf("Opcao invalida");
+        printf("\n");
+        printf("Tente novamente");
+        printf("\n");
+    }
+}
+void exibemenualterarcontato(){
     int i;
     char nomeescolhido[LIMIT];
     char sobrenomeescolhido[LIMIT];
@@ -124,7 +212,7 @@ void exibemenucontato(){
                 printf("Numero Encontrado");
                 printf("\n");
                 printf("Digite o oq  voce quer mudar no(a) %s %s: ",lista_todos_nomes[i],lista_todos_sobrenomes[i]);
-                printf("Digite 1 para alterar o nome, digite 2 para alterar o sobrenome, digite 3 para alterar o numero: ");
+                printf("Digite 1 para alterar o nome, digite 2 para alterar o sobrenome, digite 3 para alterar o numero:  ");
                 scanf("%d",&numerodigitado2);
                  switch (numerodigitado2)
                     {
@@ -144,12 +232,14 @@ void exibemenucontato(){
         printf("Opcao invalida");
         printf("\n");
         printf("Tente novamente");
+        printf("\n");
     }
 }
 int exibemenu(){
     int numerodigitado = 0;
     printf("Bem vindo a sua agenda\n");
     printf("------------------------------------\n");
+    system("cls");
     printf("Digite 9 se voce quiser encerrar o sistema\nDigite 1 se quiser adicionar um contato\nDigite 2 se voce quiser editar um contato\nDigite 3 se voce quiser apagar um contato\nDigite 4 se voce quiser buscar um contato\nDigite 5 para ver tudo: ");
     scanf("%d",&numerodigitado);
     return numerodigitado;
@@ -164,41 +254,24 @@ int main(){
             return 0;
         case 1:
             adicionacontato();
-            printf("Nomes");
-            printf("\n");
-            printaosnomes();
-            printf("\n");
-            printf("Sobrenomes");
-            printf("\n");
-            printaossobrenomes();
-            printf("\n");
-            printf("Numeros");
-            printf("\n");
-            printaosnumeros();
-            printf("\n");
             printf("Cadastro feito!");
             printf("\n");
             break;
         case 2:
-            exibemenucontato();
+            exibemenualterarcontato();
+            break;
+        case 3:
+            exibemenuapagarcontato();
+            break;
+        case 4:
+            buscacontato();
+            break;
         case 5:
-            printf("Nomes");
-            printf("\n");
-            printaosnomes();
-            printf("\n");
-            printf("Sobrenomes");
-            printf("\n");
-            printaossobrenomes();
-            printf("\n");
-            printf("Numeros");
-            printf("\n");
-            printaosnumeros();
-            printf("\n");
+            printnatela();
         default:
             break;
         }
         retorno = exibemenu();
     }
-    
     return 0;
 }
